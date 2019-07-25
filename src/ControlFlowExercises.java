d6f13e583de42dabcc6197882e368b01034fab31
victorpena13 <victor.pena1392@gmail.com>
2019-07-25 16:13:05 -0500

    lopp to count backwards by 5's from 100 to -10

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,16 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-       int i = 0;
+//        int i = 5;
+//        while (i < 15) {
+//            System.out.println(i);
+//            i++;
+//        }
+          int i = 100;
        do {
            System.out.println(i);
-           i += 2;
-       } while (i <= 100);
+           i -= 5;
+       } while (i >= -10);
+
+
     }
 }
