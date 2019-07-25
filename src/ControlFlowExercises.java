bd77556bac981d49e8c7bd182404c1a429bcaf0c
victorpena13 <victor.pena1392@gmail.com>
2019-07-25 15:58:08 -0500

    2nd exercise completed

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,9 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while (i < 15) {
-            System.out.println(i);
-            i++;
-        }
+       int i = 2;
+       do {
+           System.out.println(i);
+           i += 2;
+       } while (i <= 100);
     }
 }
