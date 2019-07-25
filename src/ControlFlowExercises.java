a6d203657d91d8e0d3179516c26ab3e4a1a0cfa3
victorpena13 <victor.pena1392@gmail.com>
2019-07-25 16:48:36 -0500

    FizzBuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -5,18 +5,45 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //            i++;
 //        }
-
+//
 //          int i = 100;
 //       do {
 //           System.out.println(i);
 //           i -= 5;
 //       } while (i >= -10);
+//
+//        long i = 2;
+//        do {
+//            System.out.println(i);
+//            i *= i;
+//        } while (i < 1000000);
+//
+//        for(int i = 0; i < 102; i+= 2) {
+//            System.out.println(i);
+//        }
+//
+//        for(int i = 5; i < 15; i++) {
+//            System.out.println(i);
+//        }
+//
+//        for(long i = 2; i < 1000000; i*=i){
+//            System.out.println(i);
+//        }
 
-        long i = 2;
-        do {
-            System.out.println(i);
-            i *= i;
-        } while (i < 1000000);
+        for(int i = 1; i < 100; i++) {
+            if(i % 5 == 0 && i % 3 ==0){
+                System.out.println("fizzbuzz");
+            } if(i % 5 == 0) {
+                System.out.println("buzz");
+            } if(i % 3 == 0) {
+                System.out.println("fizz");
+            } else {
+                System.out.println(i);
+            }
 
+
+
+
+        }
     }
 }
