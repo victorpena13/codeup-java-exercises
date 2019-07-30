85cf0f6f6afc84f6016067700c2a5b98c02243a5
victorpena13 <victor.pena1392@gmail.com>
2019-07-29 20:49:53 -0500

    completed assignment

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,15 +1,21 @@
+import java.sql.SQLOutput;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        int myFavoriteNumber;
-        myFavoriteNumber = 144000;
+        int myFavoriteNumber = 13;
         System.out.println(myFavoriteNumber);
 
-        String myString = "Betelgeuse";
+        String myString = "This is string value!";
         System.out.println(myString);
 
-        double myNumber = 3.14;
+        float myNumber = 3.14F;
         System.out.println(myNumber);
+
+
+        int y = 5;
+        System.out.println(y++);
+        System.out.println(y);
+
 
         int x = 5;
         System.out.println(++x);
@@ -17,31 +23,74 @@ public class HelloWorld {
 
 //        String theNumberThree = "three";
 //        Object o = theNumberThree;
-//        Integer three = (Integer) o;
 //        int three = (int) "three";
 
-        int y = 4;
-        y += 5;
+        int a = 4;
+        a += 4;
+        System.out.println(a);
 
-        int a = 3;
-        int b = 4;
-        b *= a;
-        // b = b * a
+        int b = 3;
+        int c = 4;
+        b *= c;
+        System.out.println();
 
-        int c = 10;
-        int d = 2;
-        //c = c / d;
-        c /= d;
-        //d = d - c;
-        d -= c;
+        int d = 10;
+        int e = 2;
 
-        int max = Integer.MAX_VALUE;
-        System.out.println(max);
-//        max+= 10;
-        System.out.println(max);
 
-        byte little = (byte)max;
-        System.out.println(little);
+
     }
 }
+
+
+
+
+
+//
+//public class HelloWorld {
+//    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+//        int myFavoriteNumber;
+//        myFavoriteNumber = 144000;
+//        System.out.println(myFavoriteNumber);
+//
+//        String myString = "Betelgeuse";
+//        System.out.println(myString);
+//
+//        double myNumber = 3.14;
+//        System.out.println(myNumber);
+//
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//
+////        String theNumberThree = "three";
+////        Object o = theNumberThree;
+////        Integer three = (Integer) o;
+////        int three = (int) "three";
+//
+//        int y = 4;
+//        y += 5;
+//
+//        int a = 3;
+//        int b = 4;
+//        b *= a;
+//        // b = b * a
+//
+//        int c = 10;
+//        int d = 2;
+//        //c = c / d;
+//        c /= d;
+//        //d = d - c;
+//        d -= c;
+//
+//        int max = Integer.MAX_VALUE;
+//        System.out.println(max);
+////        max+= 10;
+//        System.out.println(max);
+//
+//        byte little = (byte)max;
+//        System.out.println(little);
+//    }
+//}
 
