c5905c9cdc4d5348bbb30af9f5f8948e75978a58
victorpena13 <victor.pena1392@gmail.com>
2019-07-31 14:36:38 -0500

    completed

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
-
 public class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void sayHello() {
+        System.out.printf("Hello, my name is %s", this.name);
+    }
+
+    public static void main(String[] args) {
+
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+//
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+
+
+        Person cody = new Person("Cody the Duck");
+        cody.sayHello();
+    }
 }
