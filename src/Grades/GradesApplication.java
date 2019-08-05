2d8032cb0e998de113288d0ae86fe9ddc44ed8a9
victorpena13 <victor.pena1392@gmail.com>
2019-08-05 15:59:01 -0500

    completed second exercise

--- src/Grades/GradesApplication.java
+++ src/Grades/GradesApplication.java
@@ -12,6 +12,23 @@ public class GradesApplication {
         victor.addGrade(75);
         students.put("victorpena13", victor);
         System.out.println(students.get("victorpena13").getName());
+        System.out.println(students);
         System.out.println(victor.getGradeAverage());
+        Student mario = new Student("mario");
+        mario.addGrade(99);
+        mario.addGrade(75);
+        mario.addGrade(50);
+        students.put("mylittlepony25", mario);
+        System.out.println(students.get("mylittlepony25").getName());
+//        System.out.println(students);
+        System.out.println(mario.getGradeAverage());
+        Student susan = new Student("susan");
+        susan.addGrade(99);
+        susan.addGrade(89);
+        susan.addGrade(79);
+        students.put("kpopqueen52", susan);
+        System.out.println(students.get("kpopqueen52").getName());
+        System.out.println(susan.getGradeAverage());
+
     }
 }
