07b5a1a3c52eb279718d5fd4e2f5b542c4d12dc5
victorpena13 <victor.pena1392@gmail.com>
2019-08-06 09:03:58 -0500

    do while loop implemented

--- src/Grades/GradesApplication.java
+++ src/Grades/GradesApplication.java
@@ -30,17 +30,25 @@ public class GradesApplication {
         System.out.println(students.get("kpopqueen52").getName());
         System.out.println(susan.getGradeAverage());
 
-        System.out.println("Here are the GitHub Usernames of our students:");
-        System.out.println(students.keySet());
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("What student would you like to see more information on?");
-        String userInput = scanner.nextLine();
-        System.out.println(userInput);
-        if(userInput.equalsIgnoreCase("victorpena13")) {
-            System.out.println(victor.getGradeAverage());
-        } else if (userInput.equalsig)
+        boolean repeat = true;
+        do {
+            System.out.println("Here are the GitHub Usernames of our students:");
+            System.out.println(students.keySet());
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("What student would you like to see more information on?");
+            String userInput = scanner.nextLine();
+            System.out.println(userInput);
+            if(userInput.equalsIgnoreCase("victorpena13")) {
+                System.out.println(victor.getGradeAverage());
 
-
+            } else if (userInput.equalsIgnoreCase("mylittlepony25")) {
+                System.out.println(mario.getGradeAverage());
+            } else if (userInput.equalsIgnoreCase("kpopqueen52")) {
+                System.out.println(susan.getGradeAverage());
+            }
+        } while(repeat);
 
     }
 }
+
+
