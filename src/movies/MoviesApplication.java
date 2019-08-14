5ae7c859cea37296e7f6583d9c7593c59803a72d
victorpena13 <victor.pena1392@gmail.com>
2019-08-14 09:44:19 -0500

    added categories method

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -5,17 +5,29 @@ public class MoviesApplication {
 
     public static void main(String[] args) {
         Movie[] movie = MoviesArray.findAll();
-        System.out.println(displayMovies(movie));
+        System.out.println(displayAllMovies(movie));
+        System.out.println();
+        System.out.println(displayMovieByCategory(movie));
 
         }
 
-        public static String displayMovies(Movie[] movie) {
+        public static String displayMovieByCategory(Movie[] movie) {
+        String moviesCat = "";
+        for (int i = 0; i < movie.length; i++) {
+            moviesCat += (movie[i].getCategory() + " ");
+        }
+        return  moviesCat;
+        }
+
+        public static String displayAllMovies(Movie[] movie) {
         String movies = "";
         for (int i = 0; i < movie.length; i++) {
             movies += (movie[i].getName() + " - " + movie[i].getCategory());
         }
         return movies;
      }
+
+
 
     }
 
