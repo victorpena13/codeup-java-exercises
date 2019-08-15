c4b85ef1c97f384961f547e9ac4a5e0d8482b9c0
victorpena13 <victor.pena1392@gmail.com>
2019-08-14 20:21:04 -0500

    added phrases to give a user options

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,15 +1,22 @@
 package movies;
 
+import java.util.Scanner;
+
 public class MoviesApplication {
+    Scanner scanner = Scanner(System.in);
+
 
 
     public static void main(String[] args) {
         Movie[] movies = MoviesArray.findAll();
-        System.out.println(displayAllMovies(movies, ""));
-        System.out.println(displayAllMovies(movies, "drama"));
+//        System.out.println(displayAllMovies(movies, ""));
+////        System.out.println(displayAllMovies(movies, "drama"));
+        System.out.println("To Display all movies type 'All':");
+        System.out.println("To Display movies by category type: 'Drama, 'Comedy', 'Musical', 'Horror', 'SciFi, 'Animated':");
+
+
 
         }
-
 
     public static String displayAllMovies(Movie[] movies, String category) {
         String moviesAllText = "";
@@ -17,18 +24,15 @@ public class MoviesApplication {
         for (int i = 0; i < movies.length; i++) {
             if(category.isEmpty()){
                 for(Movie n : MoviesArray.findAll()){
-                    moviesAllText += (n.getName() + " " + n.getCategory() + " ") + " | ";
+                    moviesAllText += (n.getName() + " - " + n.getCategory() + " ") + "\n";
                 }
                 return moviesAllText;
             }
             if (movies[i].getCategory().equalsIgnoreCase(category)){
-                moviesText += (movies[i].getName() + " - " + movies[i].getCategory() + " ") + " | ";
+                moviesText += (movies[i].getName() + " - " + movies[i].getCategory() + " ") + " \n ";
             }
         }
         return moviesText;
     }
-
-
-
-    }
+}
 
