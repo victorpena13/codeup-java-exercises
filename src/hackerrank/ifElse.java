50e8dbec4a4be47181175276cdba8785b1d0d589
victorpena13 <victor.pena1392@gmail.com>
2020-03-21 21:02:43 -0500

    completed hackerrank 3rd challenge

---
+++ src/hackerrank/ifElse.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ifElse {
+    public static void main(String[] args) {
+
+
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    String ans = "";
+
+    if (n % 2 == 0 && (n >= 6 && n <= 20) || n % 2 != 0) {
+        ans = "Weird";
+    }
+    else if (n % 2 == 0 && (n >= 2 && n <= 5) || (n % 2 == 0 && n > 20)) {
+        ans = "Not Weird";
+        }
+    System.out.println(ans);
+    }
+}
