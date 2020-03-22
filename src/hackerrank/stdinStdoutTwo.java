83d6cdce6139838236614215c938ff8f38af9ce7
victorpena13 <victor.pena1392@gmail.com>
2020-03-22 08:35:33 -0500

    completed hackerranks 4th challenge

---
+++ src/hackerrank/stdinStdoutTwo.java
@@ -0,0 +1,21 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class stdinStdoutTwo {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String i = scan.nextLine();
+
+        String d = scan.nextLine();
+        String s = scan.nextLine();
+
+        System.out.printf("String " + s);
+
+        System.out.printf("Double: " + d);
+        System.out.printf("Int: " + i);
+
+    }
+ }
