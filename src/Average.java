2bb57b16fbdae4b9fd6abc5eba58d3d9e4b9a605
victorpena13 <victor.pena1392@gmail.com>
2020-03-21 20:22:17 -0500

    second exercise on hackerrank

---
+++ src/Average.java
@@ -0,0 +1,16 @@
+public class Average {
+
+
+
+    public static void main(String[] args) {
+        int[] numbers = {10, 12, 13};
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+
+        for(int i = 0; i < numbers.length; i++) {
+
+        }
+    }
+
+}
