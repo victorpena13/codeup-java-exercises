4b318de99332dac8297f99609d8df80312fea7a8
victor pena <victor.pena1392@Gmail.com>
2022-02-13 13:40:51 -0600

    used a do loop to square a number till it iterates to a million

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,9 @@
 public class ControlFlowExercises {
     public static void main (String[] args) {
-        int i = 105;
+        int i = 2;
         do {
-            i = i - 5;
             System.out.println(i);
-        } while (i> -10);
+            i = i *2;
+        } while (i <= 65536);
     }
 }
