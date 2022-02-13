7d44a44a462269d496866c3f029ca3a4d96eeaa7
victor pena <victor.pena1392@Gmail.com>
2022-02-13 13:58:15 -0600

    fizzbuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,21 @@
 public class ControlFlowExercises {
     public static void main (String[] args) {
-        int i = 2;
-        do {
+
+        // write a program that prints the numbers from 1 to 100.
+        // for multiples of three: print "fizz" instead of the number.
+        // for multiples of five: print "buzz".
+        // for numbers which are multiples both three and five: print "fizzbuzz"
+
+
+        for (int i =1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                System.out.println("fizz");
+                continue;
+            } else if (i % 5 == 0) {
+                System.out.println("buzz");
+                continue;
+            }
             System.out.println(i);
-            i = i *2;
-        } while (i <= 65536);
+        }
     }
 }
