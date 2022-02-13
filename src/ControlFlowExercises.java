8e5fc13c1bde1fa6fabbad0129d3269e4fa85265
victor pena <victor.pena1392@Gmail.com>
2022-02-12 20:27:13 -0600

    do while loop even numbers from 0 to 100

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,11 @@
 public class ControlFlowExercises {
     public static void main (String[] args) {
-        int i = 5;
-        while (i <= 15) {
-            System.out.println(i);
+        int i = 0;
+        do {
+            if(i % 2 == 0){
+                System.out.println(i);
+            }
             i++;
-        }
+        } while (i <= 100);
     }
 }
