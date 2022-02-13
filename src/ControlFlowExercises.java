77b54a91d97451ea1b4295206086c9f56a049320
victor pena <victor.pena1392@Gmail.com>
2022-02-13 13:23:39 -0600

    used a do loop to iterate from 100 to -10 in mulitples of 5

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,11 +1,9 @@
 public class ControlFlowExercises {
     public static void main (String[] args) {
-        int i = 0;
+        var i = 105;
         do {
-            if(i % 2 == 0){
-                System.out.println(i);
-            }
-            i++;
-        } while (i <= 100);
+            i = i - 5;
+            System.out.println(i);
+        } while (i> -10);
     }
 }
