25eaae0a3e74e58e2b0113ff5f0e8a3a07adc85d
victor pena <victor.pena1392@Gmail.com>
2022-02-14 11:33:51 -0600

    added comments with instructions for exercise

--- src/Bob.java
+++ src/Bob.java
@@ -1,4 +1,11 @@
 public class Bob {
+//    Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+//
+//    Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
+//    He answers 'Whoa, chill out!' if you yell at him. (the input ends with an exclamation mark)
+//    He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
+//    He answers 'Whatever.' to anything else.
+//    Write the Java code necessary so that a user of your command line application can have a conversation with Bob.
     public static void main(String[] args) {
 
     }
