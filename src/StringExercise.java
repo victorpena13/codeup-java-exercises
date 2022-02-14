582cbc4f142de11308896bbff32b2ab215c5acaf
victor pena <victor.pena1392@Gmail.com>
2022-02-14 11:23:26 -0600

    string basics exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,14 @@
+public class StringExercise {
+
+    public static void main (String[] args) {
+        String message = "we don't need no education";
+        System.out.println(message);
+
+        message = "we don't need no though control";
+        System.out.println(message);
+
+        message = "check \"this\" out!, \"s inside of \"s!";
+        System.out.println(message);
+    }
+
+}
