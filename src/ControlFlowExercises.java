4e10b5a50b03e5b0b2d73a122914520b9a881a0c
victor pena <victor.pena1392@Gmail.com>
2022-02-13 20:03:14 -0600

    solved fizzbuzz riddle

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,19 +1,12 @@
 public class ControlFlowExercises {
     public static void main (String[] args) {
-
-        // write a program that prints the numbers from 1 to 100.
-        // for multiples of three: print "fizz" instead of the number.
-        // for multiples of five: print "buzz".
-        // for numbers which are multiples both three and five: print "fizzbuzz"
-
-
-        for (int i =1; i <= 100; i++) {
-            if (i % 3 == 0) {
+        for(int i=1; i<100; i++) {
+            if((i%3==0) && (i%5==0)){
                 System.out.println("fizz");
-                continue;
-            } else if (i % 5 == 0) {
+            } else if (i%3==0) {
                 System.out.println("buzz");
-                continue;
+            } else if (i%5==0) {
+                System.out.println("fizzbuzz");
             }
             System.out.println(i);
         }
