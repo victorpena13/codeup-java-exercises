5f4964c5df7bcc09722ef23ed1a2c4bbcf1f85bc
victor pena <victor.pena1392@Gmail.com>
2022-02-21 23:20:54 -0600

    created sayHello Method

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,6 +1,12 @@
+import java.io.PrintStream;
+
 public class MethodsExercises {
 
-    public static void main(String[] args) {
+    public static PrintStream sayHello(String name) {
+        return System.out.printf("Hello, %s!", name);
+    }
 
+    public static void main(String[] args) {
+        sayHello("world");
     }
 }
