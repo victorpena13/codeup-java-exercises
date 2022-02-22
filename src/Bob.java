d2c490428db822b8f9b7eac07fe9766a8145c3a1
victor pena <victor.pena1392@Gmail.com>
2022-02-21 22:58:41 -0600

    can use methods .lastIndexOf and .endsWith to determine if the user is asking a question

--- src/Bob.java
+++ src/Bob.java
@@ -1,12 +1,26 @@
+import java.util.Scanner;
+
+
 public class Bob {
 //    Bob is a lackadaisical teenager. In conversation, his responses are very limited.
 //
 //    Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
+    // lastIndexOf()
+    // endsWith()
 //    He answers 'Whoa, chill out!' if you yell at him. (the input ends with an exclamation mark)
+    // lastIndexOf()
 //    He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
+    // isEmpty()
 //    He answers 'Whatever.' to anything else.
+    // else 'whatever'
 //    Write the Java code necessary so that a user of your command line application can have a conversation with Bob.
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Start a convo with bob: ");
+        //use .nextLine to capture the end of the string with methods like .endsWith();
+        String userInput = scanner.nextLine();
+        System.out.println(userInput.endsWith("?"));
+        System.out.println(userInput.lastIndexOf("?"));
     }
 }
