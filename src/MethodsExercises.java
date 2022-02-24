1416f18c0a9ac75d6c5e4daaba8ff556282858ad
victor pena <victor.pena1392@Gmail.com>
2022-02-24 15:34:24 -0600

    added examples of methods that return strings and ints

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,12 +1,22 @@
+import java.util.Locale;
+
 public class MethodsExercises {
     public static String sayHello(String name) {
         return String.format("Hello, %s!", name);
     }
 
-    public static void main(String[] args) {
-        System.out.println(sayHello("world"));;
+    public static int returnThree() {
+        return 3;
     }
 
+    public static String shout(String s) {
+        return s.toUpperCase() + "!!!";
+    }
 
+    public static void main(String[] args) {
+        System.out.println(sayHello("world"));
+        System.out.println(returnThree());
+        System.out.println(shout("does this work?"));
+    }
 
 }
