92ac7fbaf8897f95df7ce34105532bc50cf8576d
victor pena <victor.pena1392@Gmail.com>
2022-02-24 15:05:22 -0600

    placed method before main

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,11 +1,12 @@
 public class MethodsExercises {
-    public static void main(String[] args) {
-        System.out.println(sayHello("world"));;
-
-    }
-
     public static String sayHello(String name) {
         return String.format("Hello, %s!", name);
     }
+
+    public static void main(String[] args) {
+        System.out.println(sayHello("world"));;
+    }
+
+
 
 }
