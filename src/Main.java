a2484578074781e9b101420e2b8779b8615adb01
victor pena <victor.pena1392@Gmail.com>
2022-03-30 14:57:30 -0500

    declared String variable with my string. and then intialized my string and printed to the console

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-//Create an int variable named myFavoriteNumber
+// Create an int variable named myFavoriteNumber
 // and assign your favorite number to it, then print it
 // out to the console.
+
         int myFavoriteNumber = 13;
         System.out.println("My Favorite Number: " + myFavoriteNumber + ".");
+
+// Create a String variable named myString and assign a
+// string value to it, then print the variable out to the
+// console.
+
+        String myString;
+        myString = "This is a string value example";
+        System.out.println(myString);
     }
 }
