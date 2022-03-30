46c5f363492bb0ba7a783f540d6085be9e4f1618
victor pena <victor.pena1392@Gmail.com>
2022-03-30 14:52:57 -0500

    created an int variable called myFavorite number and initialized it with the number 13

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+//Create an int variable named myFavoriteNumber
+// and assign your favorite number to it, then print it
+// out to the console.
+        int myFavoriteNumber = 13;
+        System.out.println("My Favorite Number: " + myFavoriteNumber + ".");
+    }
+}
