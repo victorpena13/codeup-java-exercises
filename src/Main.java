d472ef56145d1fd4a5c801e5a85b910656aa3d38
victor pena <victor.pena1392@Gmail.com>
2022-03-30 19:43:57 -0500

    added notes

--- src/Main.java
+++ src/Main.java
@@ -46,11 +46,16 @@ public class Main {
 //  (Note the 'L' at the end) to myNumber.
 
         myNumber = 123L;
+
+//  Change your code to assign the value 123 to myNumber
+
         myNumber = 123;  // variable is already assigned to this value;
 
+//  Why does assigning the value 3.14 to a variable declared as a long
+//  not compile, but assigning an integer value does?
 
-
-
+        //because long is reserved for large numbers that range from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+        // and integer is reserved for any type(decimal or whole) of number between a smaller range -2,147,483,648 to 2,147,483,647
 
     }
 }
