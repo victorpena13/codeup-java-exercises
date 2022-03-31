b0da7ce3bdf33a08c8a71a7b05fc61c583865f4d
victor pena <victor.pena1392@Gmail.com>
2022-03-31 15:19:07 -0500

    introduced scanner

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
 
 public class ConsoleExercises {
     public static void main(String[] args) {
 
+        String name = "codeup";
+        System.out.printf("hello there, %s. Nice to see you.%n", name);
+
+        String greeting = "salutations";
+        System.out.printf("%s, %s!", greeting, name);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter something: ");
+        String userInput = scanner.next();
+
+        System.out.println("You entered: --> \"" + userInput + "\" <--");
 
     }
 }
