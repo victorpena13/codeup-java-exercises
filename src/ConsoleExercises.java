cf806fe22cd94a424873cde35226fc466c5cd467
victor pena <victor.pena1392@Gmail.com>
2022-03-31 16:03:32 -0500

    used .next to capture 3 words

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -3,8 +3,30 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
         double pi = 3.14159;
-        System.out.printf("The value of pi is approximately %.2f%n", pi); // %.2f shortens to two decimal places.  %n breaks line
+        System.out.printf("The value of pi is approximately %.2f%n", pi); // %.2f shortens to two decimal places.  %n breaks line.
+
+
+        System.out.print("enter an integer: ");
+        int userInteger = scanner.nextInt();
+        System.out.println(userInteger);
+
+        System.out.println("enter 3 words: ");
+        String userWordOne = scanner.next(); // captures only one word
+        String userWordTwo = scanner.next(); // captures every word after the previous
+        String userWordThree = scanner.next();
+
+        System.out.println(userWordOne);
+        System.out.println(userWordTwo);
+        System.out.println(userWordThree);
+
+
+
+
+
+
 
 
 
