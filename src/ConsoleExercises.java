e0142f7bc50bd71a1206b137c1dbdf94eac88357
victor pena <victor.pena1392@Gmail.com>
2022-04-02 15:10:43 -0500

    used a for loop to square itself from 2 - 1000000

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,12 +1,10 @@
 public class ConsoleExercises {
     public static void main(String[] args) {
-
         int a = 5;
         while(a<=15) {
             System.out.println(a);
             ++a;
         }
-
         int b = 0;
         do {
                 if(b % 2 == 0) {
@@ -14,13 +12,14 @@ public class ConsoleExercises {
                 }
                 b++;
         } while (b<=100);
-
         int c = 100;
         do {
             System.out.println(c);
             c-=5;
         } while(c>0);
 
-
+        for (long d = 2; d < 1000000; d *= d) {
+            System.out.println(d);
+        }
     }
 }
