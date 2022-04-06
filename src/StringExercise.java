098449b49153f889c39aead2d6c7e06d2f45fe55
victor pena <victor.pena1392@Gmail.com>
2022-04-06 11:19:42 -0500

    introduced .equals method to check if the values of two string variables match

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,11 +1,26 @@
 // strings are objects - meaning they are a reference type.
 // == operator checks if both objects are the same reference.
 // ==  therefore you can't use this to compare strings only object type.
+// strings are immutable
+
+import java.util.Scanner;
 
 public class StringExercise {
     public static void main (String[] args) {
+        String message = "Hello";
+        System.out.println(message);
+        message = message + " World!";
+        System.out.println(message);
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Continue? [y,N]");
+        String userInput = scanner.next();
 
+        if(userInput.equals("y")){
+            System.out.println("yes");
+        } else if (userInput.equals("n")) {
+            System.out.println("no");
+        }
 
     }
 
