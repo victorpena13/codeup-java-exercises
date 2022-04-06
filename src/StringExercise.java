77fdde4e5cddb5e3e952002a27e094aeb3f998b4
victor pena <victor.pena1392@Gmail.com>
2022-04-06 09:06:14 -0500

    notes about strings on lines 1-3

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,5 +1,11 @@
+// strings are objects - meaning they are a reference type.
+// == operator checks if both objects are the same reference.
+// ==  therefore you can't use this to compare strings only object type.
+
 public class StringExercise {
     public static void main (String[] args) {
+
+
 
     }
 
