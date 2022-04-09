f8fcb8db7e294b74e6aacefff33459664dc67ad7
victor pena <victor.pena1392@Gmail.com>
2022-04-09 00:24:37 -0500

    printed string variable

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,2 +1,9 @@
 public class StringExercise {
+    public static void main(String[] args) {
+        String pinkFloydAnotherBrickInTheWall = "We don't need no education \nWe don't need no thought control";
+        System.out.println(pinkFloydAnotherBrickInTheWall);
+
+
+
+    }
 }
