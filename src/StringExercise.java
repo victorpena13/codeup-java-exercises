75d2737dc5c1ae73b3ae8c8b8310958142c2eeb3
victor pena <victor.pena1392@Gmail.com>
2022-04-09 00:32:23 -0500

    escaped multiple backslashes with String variable escapeMultipleBackslashes on line 12

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -5,5 +5,11 @@ public class StringExercise {
 
         String quotations = "Check \"this\" out!, \"s inside of \"s!";
         System.out.println(quotations);
+
+        String escapeSingleBackslash = "In windows, the main drive is usually C:\\";
+        System.out.println(escapeSingleBackslash);
+
+        String escapeMultipleBackslashes = "I can do backslashes\\, double backslashes \\\\,\nand the amazing triple backslash\\\\\\!";
+        System.out.println(escapeMultipleBackslashes);
     }
 }
