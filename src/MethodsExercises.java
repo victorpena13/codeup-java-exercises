b8f1f2aed5cfe12d8f06b0fc5b5d4e8b010d0833
victor pena <victor.pena1392@Gmail.com>
2022-04-11 09:17:33 -0500

    added notes for multiplication bonus problem

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,6 +1,6 @@
 public class MethodsExercises {
 
-    public static long addition(long x, long y) {
+    public static int addition(int x, int y) {
         return x+y;
     }
 
@@ -9,16 +9,25 @@ public class MethodsExercises {
     }
 
     public static int multiplication(int x, int y) {
-        return x*y;
+            return x *y;
+            // for bonus you wil use shift and plus operators.
+        }
+
+    public static int division(int x, int y) {
+        return x / y;
     }
 
+    public static int modulus(int x, int y) {
+        return x % y;
 
+    }
 
     public static void main(String[] args) {
         System.out.println(addition(1, 4));
         System.out.println(subtraction(0,1));
-
         System.out.println(multiplication(3,3));
+        System.out.println(division(4,2));
+        System.out.println(modulus(4,2));
     }
 
 }
