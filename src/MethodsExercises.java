4da9ea854a025f40d252ec106cee9d71d3b7192a
victor pena <victor.pena1392@Gmail.com>
2022-04-11 08:50:09 -0500

    created a method called subtraction that accepts two arguments and subtracts

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -4,8 +4,21 @@ public class MethodsExercises {
         return x+y;
     }
 
+    public static int subtraction(int x, int y) {
+        return x-y;
+    }
+
+    public static int multiplication(int x, int y) {
+        return x*y;
+    }
+
+
+
     public static void main(String[] args) {
         System.out.println(addition(1, 4));
+        System.out.println(subtraction(0,1));
 
+        System.out.println(multiplication(3,3));
     }
+
 }
