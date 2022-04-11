05db804bddc0b17c63c5569d7f7494748c55ee56
victor pena <victor.pena1392@Gmail.com>
2022-04-10 22:52:49 -0500

    sayHello method. printed sayHello method

--- src/MethodsExerciseLesson.java
+++ src/MethodsExerciseLesson.java
@@ -1,2 +1,11 @@
 public class MethodsExerciseLesson {
+
+    public static String sayHello(String name) {
+        return String.format("Hello, %s!", name);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sayHello("Victor"));
+
+    }
 }
