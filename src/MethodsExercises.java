8790784f89873cc6d0e64daeb0e0dc716125866d
victor pena <victor.pena1392@Gmail.com>
2022-04-11 08:43:37 -0500

    created simple method that adds to arguments togehter

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,5 +1,11 @@
 public class MethodsExercises {
+
+    public static long addition(long x, long y) {
+        return x+y;
+    }
+
     public static void main(String[] args) {
+        System.out.println(addition(1, 4));
 
     }
 }
