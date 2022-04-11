cb1fde6d69b2333e396c3ad4225cb8bde2addd77
victor pena <victor.pena1392@Gmail.com>
2022-04-10 22:56:25 -0500

    string method with two arguments

--- src/MethodsExerciseLesson.java
+++ src/MethodsExerciseLesson.java
@@ -4,8 +4,14 @@ public class MethodsExerciseLesson {
         return String.format("Hello, %s!", name);
     }
 
+    public static String sup(String greeting, String name) {
+        return String.format("%s, %s!\n", greeting, name);
+    }
+
     public static void main(String[] args) {
         System.out.println(sayHello("Victor"));
+        System.out.println(sup("whats up", "dude"));
 
     }
+
 }
