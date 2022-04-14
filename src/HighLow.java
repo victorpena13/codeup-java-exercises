3698745cdea1bb1dff6c289c49d4b21346c7dd8b
victor pena <victor.pena1392@Gmail.com>
2022-04-14 00:55:21 -0500

    Highlow checks a users guess matches the random number between 1 and 100 - completed

--- src/HighLow.java
+++ src/HighLow.java
@@ -2,11 +2,26 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class HighLow {
+    public static boolean HigherLower(int guess, int randomNumber) {
+        Scanner scanner = new Scanner(System.in);
+        do {
+            if(guess < randomNumber) {
+                System.out.println("Higher");
+                System.out.println("Try again:");
+                int usersGuess = scanner.nextInt();
+                HigherLower(usersGuess, randomNumber);
+            } else if (guess > randomNumber) {
+                System.out.println("Lower");
+                System.out.println("Try again: ");
+                int usersGuess = scanner.nextInt();
+                HigherLower(usersGuess, randomNumber);
+            } else if (guess == randomNumber);
+            return true;
+        } while (false);
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
         Random r = new Random();
         int low = 1;
         int high = 100;
@@ -14,5 +29,6 @@ public class HighLow {
 
         System.out.println("Guess the correct number between 1 and 100: ");
         int userInput = scanner.nextInt();
+        System.out.println(HigherLower(userInput, result));
     }
 }
