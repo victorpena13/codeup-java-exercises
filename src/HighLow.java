50d59e585323cd08a5f5a7b5edc061396c91a29c
victor pena <victor.pena1392@Gmail.com>
2022-04-14 00:14:01 -0500

    imported scanner to captures user input

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,12 +1,18 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class HighLow {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
         Random r = new Random();
         int low = 1;
         int high = 100;
         int result = r.nextInt(high-low) + low;
-        System.out.println(result);
+
+        System.out.println("Guess the correct number between 1 and 100: ");
+        int userInput = scanner.nextInt();
     }
 }
