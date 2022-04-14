cec3da1c1b7bd5d86487d2c7b9dddf855cfeba82
victor pena <victor.pena1392@Gmail.com>
2022-04-14 00:11:18 -0500

    lines 6-10 generate a random number between 1 and 100

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,3 +1,12 @@
+import java.util.Random;
+
 public class HighLow {
 
+    public static void main(String[] args) {
+        Random r = new Random();
+        int low = 1;
+        int high = 100;
+        int result = r.nextInt(high-low) + low;
+        System.out.println(result);
+    }
 }
