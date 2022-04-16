51b6769d44003f437c77bb7c0dfb93a27854cd0a
victor pena <victor.pena1392@Gmail.com>
2022-04-16 13:05:10 -0500

    removed duplicate code for rick sanchez

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Person {
+public class Person {
+    public String firstName;
+    public String lastName;
+
+    public String sayHello() {
+        return String.format("Hello from %s %s!", firstName, lastName);
+    }
+
+    public static void main(String[] args) {
+        Person rick = new Person();
+        rick.firstName = "Rick";
+        rick.lastName = "Sanchez";
+
+        Person ada = new Person();
+        ada.firstName = "Ada";
+        ada.lastName = "Lovelace";
+
+        Person grace = new Person();
+        grace.firstName = "Grace";
+        grace.lastName = "Hopper";
+
+        System.out.println(rick.sayHello());
+        System.out.println(ada.sayHello());
+        System.out.println(grace.sayHello());
+
+    }
 }
