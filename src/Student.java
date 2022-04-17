32b0e6e0443dc78947db01f535c084b079056f3b
victor pena <victor.pena1392@Gmail.com>
2022-04-16 21:21:52 -0500

    introduced private fields that are only accessible within the class that defines it

--- src/Student.java
+++ src/Student.java
@@ -1,6 +1,7 @@
 public class Student {
     public String name;
     public String cohort;
+    private double grade;
 
     public Student(String studentName) {
         name = studentName;
@@ -16,11 +17,23 @@ public class Student {
         return String.format("name: %s, cohort: %s", name, cohort);
     }
 
+    public Student(String name, double grade) {
+        this.name = name;
+        this. grade = grade;
+    }
+
+    public double shareGrade() {
+        return grade;
+    }
+
     public static void main(String[] args) {
         Student s1 = new Student("Student A");
         Student s2 = new Student("Student B", "Voyagers");
         System.out.println(s1.getInfo());
         System.out.println(s2.getInfo());
-    }
 
+        s1 = new Student(s1.name, 84);
+        System.out.println(s1.name);
+        System.out.println(s1.shareGrade());
+    }
 }
