f1d1db90f144fca5417448172dce098c38921021
victor pena <victor.pena1392@Gmail.com>
2022-04-16 21:53:55 -0500

    testing Person class

--- src/Person.java
+++ src/Person.java
@@ -19,5 +19,20 @@ public class Person {
     Person victorP = new Person();
     victorP.setName("victor pena");
     victorP.sayHello();
+
+    Person person1 = new Person();
+    Person person2 = person1;
+    person1.setName("John");
+//    person2.setName("John");
+
+        System.out.println(person1.getName().equals(person2.getName()));
+        System.out.println(person1 == person2);
+
+        System.out.println(person1.getName()); //john
+        System.out.println(person2.getName()); //john
+        person2.setName("Jane");
+        System.out.println(person1.getName());  //result: is jane because person 2 is person 1
+        System.out.println(person2.getName());  //jane
+
     }
 }
