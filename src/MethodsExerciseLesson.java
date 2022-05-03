81236b49d22548aadb39791d6e18b40502b6b1a1
victor pena <victor.pena1392@Gmail.com>
2022-05-03 14:54:07 -0500

    introduced void for a method with no return

--- src/MethodsExerciseLesson.java
+++ src/MethodsExerciseLesson.java
@@ -4,6 +4,10 @@ public class MethodsExerciseLesson {
         return String.format("Hello, %s", name);
     }
 
+    public static void sayHello(String greeting, String name) {
+        System.out.printf("%s, %s!\n", greeting, name);
+    }
+
     public static void main(String[] args) {
         System.out.println(sayHello("victor"));
     }
