c4ca2b139da96d62cb9d7e79a51a3865f296f792
victor pena <victor.pena1392@Gmail.com>
2022-05-03 15:12:45 -0500

    more examples of method overloading

--- src/MethodsExerciseLesson.java
+++ src/MethodsExerciseLesson.java
@@ -1,11 +1,21 @@
 public class MethodsExerciseLesson {
 
-    public static String sayHello(String name) {
-        return String.format("Hello, %s", name);
+    public static void sayHello() {
+         sayHello("Hello", "World!");
+    }
+
+    public static void sayHello(int times) {
+        for (int i =0; i < times; i += 1) {
+            sayHello();
+        }
+    }
+
+    public static void sayHello(String name) {
+      sayHello("Hello, ", name);
     }
 
     public static void sayHello(String greeting, String name) {
-        System.out.printf("%s, %s!\n", greeting, name);
+        System.out.println(greeting + " " + name);
     }
 
     public static int returnThree() {
@@ -17,9 +27,12 @@ public class MethodsExerciseLesson {
     }
 
     public static void main(String[] args) {
-        System.out.println(sayHello("victor"));
+        sayHello(5);
+        sayHello("victor");
         sayHello("victor can overload a", "method");// no need to sout because it is inside method
         System.out.println(returnThree());
         System.out.println(shout("these letters should be capitalized!"));
     }
+
+
 }
