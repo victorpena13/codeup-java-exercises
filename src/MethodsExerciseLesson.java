f89a175c0cf9ba90d1a106ebfb1e986cad391e6d
victor pena <victor.pena1392@Gmail.com>
2022-05-03 14:59:33 -0500

    introduced method overloading | returned an integer | introduced .toUpperCase() method

--- src/MethodsExerciseLesson.java
+++ src/MethodsExerciseLesson.java
@@ -8,7 +8,18 @@ public class MethodsExerciseLesson {
         System.out.printf("%s, %s!\n", greeting, name);
     }
 
+    public static int returnThree() {
+        return 3;
+    }
+
+    public static String shout(String s) {
+        return s.toUpperCase();
+    }
+
     public static void main(String[] args) {
         System.out.println(sayHello("victor"));
+        sayHello("victor can overload a", "method");// no need to sout because it is inside method
+        System.out.println(returnThree());
+        System.out.println(shout("these letters should be capitalized!"));
     }
 }
