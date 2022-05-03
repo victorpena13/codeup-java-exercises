d86d97ec5ff3fba48e9720f8cb5aa336f7b9517e
victor pena <victor.pena1392@Gmail.com>
2022-05-03 14:49:00 -0500

    fixed bug that was preventing class from running

--- src/MethodsExerciseLesson.java
+++ src/MethodsExerciseLesson.java
@@ -1,10 +1,10 @@
 public class MethodsExerciseLesson {
+
     public static String sayHello(String name) {
         return String.format("Hello, %s", name);
     }
 
-    public static void main(String args) {
+    public static void main(String[] args) {
         System.out.println(sayHello("victor"));
     }
-
 }
