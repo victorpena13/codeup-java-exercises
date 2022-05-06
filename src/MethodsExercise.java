85d632f6a227af0ee24f69fedb306e1f0d449239
victor pena <victor.pena1392@Gmail.com>
2022-05-05 21:52:15 -0500

    created a method that is an instance of the method prints the sum of two numbers

--- src/MethodsExercise.java
+++ src/MethodsExercise.java
@@ -1,2 +1,12 @@
 public class MethodsExercise {
+
+    public static void addition(int x,int y) {
+        System.out.println(x + y);
+
+    }
+
+    public static void main(String[] args) {
+        addition(2,5);
+    }
+
 }
