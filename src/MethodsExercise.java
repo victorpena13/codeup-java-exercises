62b68bf23a37113a56f7ffa69ef0a04a88e490e6
victor pena <victor.pena1392@Gmail.com>
2022-05-05 21:53:14 -0500

    created a method that subtracts to integers

--- src/MethodsExercise.java
+++ src/MethodsExercise.java
@@ -2,11 +2,17 @@ public class MethodsExercise {
 
     public static void addition(int x,int y) {
         System.out.println(x + y);
-
     }
+
+    public static void subtraction(int x, int y) {
+        System.out.println(x-y);
+    }
+
 
     public static void main(String[] args) {
         addition(2,5);
+        subtraction(2,5);
+
     }
 
 }
