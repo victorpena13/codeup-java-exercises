db00d190af92397e17b16cdcb16abe905c039e83
victor pena <victor.pena1392@Gmail.com>
2022-05-05 21:54:47 -0500

    created method division() that divides two parameters

--- src/MethodsExercise.java
+++ src/MethodsExercise.java
@@ -8,10 +8,20 @@ public class MethodsExercise {
         System.out.println(x-y);
     }
 
+    public static void multiplication(int x, int y) {
+        System.out.println(x*y);
+    }
+
+    public static void division(int x, int y) {
+
+    }
+
 
     public static void main(String[] args) {
         addition(2,5);
         subtraction(2,5);
+        multiplication(2,5);
+        division(4,2);
 
     }
 
