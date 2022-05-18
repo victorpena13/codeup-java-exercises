ba1b2f4f3d0dc28e89c1a146272298bc52115b54
victor pena <victor.pena1392@Gmail.com>
2022-05-18 10:15:38 -0500

    increment and decrment

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,22 @@ public class Main {
 //        myString = 3.14; // requires string and provided double
         long myNumber;
 //        System.out.println(myNumber); // error: not initialized
+//        myNumber = 3.14; // error: gave double requires long
+
+        System.out.println("Two Code Blocks Executed: ");
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+        int y = 5;
+        System.out.println(++y);
+        System.out.println(y);
+
+// What is the difference between the above code blocks?
+// Explain why the code outputs what it does.
+        // the difference between post and pre increment. is with post increment
+        // the new value is not immediately revealed.
+        // with pre increment the result is immediately revealed
+
     }
 }
