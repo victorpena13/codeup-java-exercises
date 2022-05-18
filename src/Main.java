435e25cd1fc2b6109fae66c361f937f6c2514d0c
victor pena <victor.pena1392@Gmail.com>
2022-05-18 15:28:32 -0500

    explicit casting lines 18-20

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,20 @@ public class Main {
         String myString = "This is a string";
 //        myString = 'c'; // cannot use single quotes
 //        myString = 3.14; // requires string and provided double
-        long myNumber;
+        float myNumber;
 //        System.out.println(myNumber); // error: not initialized
 //        myNumber = 3.14; // error: gave double requires long
         myNumber = 123L;
         myNumber = 123; // the number 123 is already assigned in line 11.
+
+//       Why does assigning the value 3.14 to a variable declared as a long not compile, but assigning an integer value does?
+        // because long is reserved for large numbers that range from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+        // and integer is reserved for any type(decimal or whole) of number between a smaller range -2,147,483,648 to 2,147,483,647
+
+//        Change your code to declare myNumber as a float. Assign the value 3.14 to it. What happens? What are two ways we could fix this?
+        myNumber = (float) 3.14;
+        myNumber = 3.14F;
+
 
         System.out.println("Two Code Blocks Executed: ");
         int x = 5;
