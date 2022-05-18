60afa69532d2a447393bdc6d7fd6bbe36cedcd60
victor pena <victor.pena1392@Gmail.com>
2022-05-17 23:08:49 -0500

    tested errors by trying to assign a single character and a double to a string

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 
         int myFavoriteNumber = 13;
         String myString = "This is a string";
-
+//        myString = 'c'; // cannot use single quotes
+//        myString = 3.14; // requires string and provided double
     }
 }
