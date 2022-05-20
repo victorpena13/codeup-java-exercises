798db4add91ac44ab1c43b84757854e25b9a5a6c
victor pena <victor.pena1392@Gmail.com>
2022-05-20 09:44:09 -0500

    formatted the decimal spaces in pi

--- src/ConsoleExercise.java
+++ src/ConsoleExercise.java
@@ -2,6 +2,7 @@ public class ConsoleExercise {
     public static void main(String[] args) {
 
         double pi = 3.14159;
+        System.out.format("The value of pi is approximately %.2f", pi);
 
     }
 
