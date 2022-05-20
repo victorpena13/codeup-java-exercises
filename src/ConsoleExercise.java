9521f04d8812e8a22de8520fc19889c93740745d
victor pena <victor.pena1392@Gmail.com>
2022-05-20 09:58:51 -0500

    introduced scanner.next to capture words of a string

--- src/ConsoleExercise.java
+++ src/ConsoleExercise.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class ConsoleExercise {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         double pi = 3.14159;
-        System.out.format("The value of pi is approximately %.2f", pi);
+        System.out.format("The value of pi is approximately %.2f\n", pi);
+
+        System.out.print("Enter an integer: ");
+        int userInt = scanner.nextInt(); // if integer is not passed-system throws an error
+        System.out.println("you entered " + userInt);
+
+        System.out.print("Enter three words: ");
+        String userWordOne = scanner.next();
+        String userWordTwo = scanner.next();
+        String userWordThree = scanner.next();
+
+        System.out.println("word one: " + userWordOne);
+        System.out.println("word three: " +userWordThree);
 
     }
 
