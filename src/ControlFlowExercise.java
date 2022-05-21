c40256879421cbde3ccce5e2a581b0884197c04b
victor pena <victor.pena1392@Gmail.com>
2022-05-21 10:45:38 -0500

    do while loop from 0-100 increments of 2

--- src/ControlFlowExercise.java
+++ src/ControlFlowExercise.java
@@ -1,5 +1,6 @@
 public class ControlFlowExercise {
     public static void main(String[] args) {
+        System.out.println("while loop:");
         int i = 5;
 
         while (i <= 15) {
@@ -7,6 +8,11 @@ public class ControlFlowExercise {
             i++;
         }
 
-
+        System.out.println("\ndo-while loop:");
+        int a = 0;
+        do {
+            System.out.print(a + " ");
+            a++;
+        } while (a <= 100);
     }
 }
