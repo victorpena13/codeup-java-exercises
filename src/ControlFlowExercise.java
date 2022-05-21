b36ad4b8fa5514533e780b512049f2fa9ffc7634
victor pena <victor.pena1392@Gmail.com>
2022-05-20 22:34:04 -0500

    created a while loop that iterates from 5 to 15

--- src/ControlFlowExercise.java
+++ src/ControlFlowExercise.java
@@ -2,6 +2,11 @@ public class ControlFlowExercise {
     public static void main(String[] args) {
         int i = 5;
 
+        while (i <= 15) {
+            System.out.print(i + " ");
+            i++;
+        }
+
 
     }
 }
