d2963c442375df0c67c0991fb31e512f52655a92
victor pena <victor.pena1392@Gmail.com>
2022-05-22 13:51:40 -0500

    four basic mathematical functions

--- src/MethodsExerciseTwo.java
+++ src/MethodsExerciseTwo.java
@@ -1,5 +1,24 @@
 public class MethodsExerciseTwo {
+    public static void addition(int x,int y) {
+        System.out.println(x+y);
+    }
+
+    public static void subtraction(int x, int y) {
+        System.out.println(x-y);
+    }
+
+    public static void multiplication(int x, int y) {
+        System.out.println(x*y);
+    }
+
+    public static void division(int x, int y) {
+        System.out.println(x/y);
+    }
     public static void main(String[] args) {
+        addition(2,2);
+        subtraction(4,2);
+        multiplication(2,3);
+        division(4,2);
 
     }
 }
