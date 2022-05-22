4163d14a8b77060df9598a1925d8ce791f3c10e7
victor pena <victor.pena1392@Gmail.com>
2022-05-21 21:59:46 -0500

    escaped characters

--- src/StringExercises.java
+++ src/StringExercises.java
@@ -3,6 +3,8 @@ public class StringExercises {
     public static void main(String[] args) {
         String pinkFloydAnotherBrickInTheWall = "We don't need no education\nWe don't need no thought control";
         System.out.println(pinkFloydAnotherBrickInTheWall);
+        String quotations = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(quotations);
 
     }
 
