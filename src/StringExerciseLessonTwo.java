a7c7f25f4567d525e5aaa1e5bdb5366929452288
victor pena <victor.pena1392@Gmail.com>
2022-05-21 21:32:21 -0500

    created StringExercises class

--- src/StringExerciseLessonTwo.java
+++ src/StringExerciseLessonTwo.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
+
 public class StringExerciseLessonTwo {
     public static void main(String[] args) {
         String message = "Hello";
         System.out.println(message);
-        message = message + " World!";
+        message = message + " World!";  //creating a new string object, and updating the message variable to reference the new String object instead of the old one.
         System.out.println(message);
+
+
+
     }
 }
---
+++ src/StringExercises.java
@@ -0,0 +1,2 @@
+public class StringExercises {
+}
