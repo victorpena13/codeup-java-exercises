d51472063009ea8c4810e7a837ffabd28691da0c
victor pena <victor.pena1392@Gmail.com>
2022-05-22 13:30:21 -0500

    method overloading

--- src/MethodsExerciseLessonTwo.java
+++ src/MethodsExerciseLessonTwo.java
@@ -15,9 +15,23 @@ public class MethodsExerciseLessonTwo {
     public static void sayHello(String greeting, String name) {
         System.out.printf("%s,%s!\n", greeting, name);
     }
+
+    public static void sayHello(int times) {
+        for (int i=0; i < times; i +=1) {
+            sayHello("hello", " world");
+        }
+    }
+
+    public static int returnThree() {
+        return 3;
+    }
+
+    public static String shout(String s) {
+        return s.toUpperCase() + "!!!";
+    }
     public static void main(String[] args) {
         System.out.println(sayHello("victor"));
         sayHello("test", " message");
-
+        sayHello(3);
     }
 }
