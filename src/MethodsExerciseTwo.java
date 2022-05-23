4d45f40f81b2ce2cba88971ab312d1833685eb43
victor pena <victor.pena1392@Gmail.com>
2022-05-22 22:16:25 -0500

    multiplication without *

--- src/MethodsExerciseTwo.java
+++ src/MethodsExerciseTwo.java
@@ -1,3 +1,4 @@
+
 public class MethodsExerciseTwo {
     public static void addition(int x,int y) {
         System.out.println(x+y);
@@ -11,7 +12,20 @@ public class MethodsExerciseTwo {
         System.out.println(x*y);
     }
 
+    public static int multiplicationWithoutStar(int x, int y) {
+        int result = 0;
+        for(int i =1; i<=y; i++) {
+            result = result+x;
+        }
+        return result;
+    }
+
+
     public static void division(int x, int y) {
+        if (y == 0) {
+            System.out.println("undefined");
+            return;
+        }
         System.out.println(x/y);
     }
 
@@ -23,7 +37,11 @@ public class MethodsExerciseTwo {
         subtraction(4,2);
         multiplication(2,3);
         division(4,2);
+        System.out.print("division ");
+        division(5,0);
         modulus(4,2);
+        System.out.println(multiplicationWithoutStar(3, 3));
+
 
     }
 }
