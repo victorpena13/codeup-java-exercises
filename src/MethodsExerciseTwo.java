638337c2e0a8a26200e25a712891100a1db15fa6
victor pena <victor.pena1392@Gmail.com>
2022-05-22 22:00:33 -0500

    added another modulus that calculates the modulus of two numbers

--- src/MethodsExerciseTwo.java
+++ src/MethodsExerciseTwo.java
@@ -14,11 +14,16 @@ public class MethodsExerciseTwo {
     public static void division(int x, int y) {
         System.out.println(x/y);
     }
+
+    public static void modulus(int x, int y) {
+        System.out.println(x%y);
+    }
     public static void main(String[] args) {
         addition(2,2);
         subtraction(4,2);
         multiplication(2,3);
         division(4,2);
+        modulus(4,2);
 
     }
 }
