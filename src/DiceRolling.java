0f24434a892c740efd70c893a6b8378a559735d5
victor pena <victor.pena1392@Gmail.com>
2022-05-25 16:00:17 -0500

    created a method that simulates the roll of two dice

--- src/DiceRolling.java
+++ src/DiceRolling.java
@@ -1,11 +1,20 @@
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class DiceRolling {
+
+    public static void rollDice(int sides) {
+        int dieOne = ThreadLocalRandom.current().nextInt(1, sides);
+        int dieTwo = ThreadLocalRandom.current().nextInt(1, sides);
+        System.out.println("dice 1: " + dieOne);
+        System.out.println("dice 2: " + dieTwo);
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of sides for a pair of dice: ");
         int userInteger = scanner.nextInt();
+        rollDice(userInteger);
 
 
     }
