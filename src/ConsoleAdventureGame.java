84fe2760bceadac468fcaabbc95fb1103a1d5a81
victor pena <victor.pena1392@Gmail.com>
2022-05-25 18:52:35 -0500

    defined a class named Person. with two instance variables-firstname & lastname. the class also defines an instance method, sayHello

--- src/ConsoleAdventureGame.java
+++ src/ConsoleAdventureGame.java
@@ -1,16 +1,29 @@
 import java.util.Scanner;
-
 public class ConsoleAdventureGame {
+    public String playerName;
+    public int playerHealth = 100;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Are you ready to start? [y/n] ");
         String userReady = scanner.next();
-        String username;
         if(userReady.equalsIgnoreCase("y")){
+            ConsoleAdventureGame user = new ConsoleAdventureGame();
             System.out.print("What is your name ");
-            scanner.nextLine();
-            username = scanner.nextLine();
-            System.out.println("Hello, " + username);
+            user.playerName = scanner.nextLine();
+            System.out.println("Hello, " + user + " " + user.playerHealth);
+        } else if (userReady.equalsIgnoreCase("n")){
+            System.out.println("goodbye");
+            return;
+        }
+        System.out.println("To view yours and your enemies health type \'health\' ");
+        System.out.println("To run type in \'run\'");
+        System.out.println("To attack type in \'attack\'");
+        System.out.println("To Drink potion type in \'potion\'");
+
+        System.out.print("action: ");
+        String userAction = scanner.next();
+        if(userAction.equalsIgnoreCase("health")) {
+
         }
 
     }
--- src/Person.java
+++ src/Person.java
@@ -1,38 +1,15 @@
 public class Person {
-    private String name;
+    public String firstName;
+    public String lastName;
 
-    public void setName(String fullName) {
-        name = fullName;
-    }
-
-    public String getName() {
-        return name;
-
-    }
-
-    public void sayHello() {
-        System.out.println("Hello from, " + getName());
+    public String sayhello() {
+        return String.format("Hello from %s %s!", firstName, lastName);
     }
 
     public static void main(String[] args) {
-
-    Person victorP = new Person();
-    victorP.setName("victor pena");
-    victorP.sayHello();
-
-    Person person1 = new Person();
-    Person person2 = person1;
-    person1.setName("John");
-//    person2.setName("John");
-
-        System.out.println(person1.getName().equals(person2.getName()));
-        System.out.println(person1 == person2);
-
-        System.out.println(person1.getName()); //john
-        System.out.println(person2.getName()); //john
-        person2.setName("Jane");
-        System.out.println(person1.getName());  //result: is jane because person 2 is person 1
-        System.out.println(person2.getName());  //jane
-
+        Person rick = new Person();
+        rick.firstName = "Rick";
+        rick.lastName = "Sanchez";
+        System.out.println(rick.sayhello());
     }
 }
