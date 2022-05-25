421037417025947a0e09953ebdaf2592fc0fd3f8
victor pena <victor.pena1392@Gmail.com>
2022-05-25 10:19:43 -0500

    imported scanner into DiceRolling java class

--- src/DiceRolling.java
+++ src/DiceRolling.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class DiceRolling {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of sides for a pair of dice: ");
+        int userInteger = scanner.nextInt();
+
 
     }
 
