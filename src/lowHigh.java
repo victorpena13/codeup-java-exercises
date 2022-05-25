01a3ef7ce8a3a8d97fef1df1c1ee7995183a5812
victor pena <victor.pena1392@Gmail.com>
2022-05-24 21:12:44 -0500

    generate a random number between 1 & 100

--- src/lowHigh.java
+++ src/lowHigh.java
@@ -1,5 +1,11 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class lowHigh {
+
     public static void main(String[] args) {
+        int randomInt = ThreadLocalRandom.current().nextInt(1, 101);
+        System.out.println(randomInt);
+
 
     }
 }
