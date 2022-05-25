1a7a46f8eab8e72687853281b82d9fb61888947e
victor pena <victor.pena1392@Gmail.com>
2022-05-24 22:12:35 -0500

    tells user higher or lower to guess correctly

--- src/lowHigh.java
+++ src/lowHigh.java
@@ -5,18 +5,23 @@ public class lowHigh {
 
     public static void trueOrFalse(int randomNumber, int userGuess){
         Scanner scanner = new Scanner(System.in);
-        System.out.println(randomNumber);
-
 
         do {
             System.out.println("wrong");
             System.out.print("guess the random number between 1 and 100: ");
             int userInt = scanner.nextInt();
+
+            if(randomNumber > userInt) {
+                System.out.println("Higher");
+            } else if (randomNumber < userInt) {
+                System.out.println("Lower");
+            }
             if (randomNumber == userInt) {
                 System.out.println("correct");
                 return;
             }
             trueOrFalse(randomNumber, userInt);
+
 
         } while(randomNumber != userGuess);
 
