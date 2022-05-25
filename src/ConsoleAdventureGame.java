a618e264db0f9bfdf17563fb6fa219f2b54ec56f
victor pena <victor.pena1392@Gmail.com>
2022-05-25 18:19:53 -0500

    ConsoleAdventureGames

--- src/ConsoleAdventureGame.java
+++ src/ConsoleAdventureGame.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
 
 public class ConsoleAdventureGame {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Are you ready to start? [y/n] ");
+        String userReady = scanner.next();
+        String username;
+        if(userReady.equalsIgnoreCase("y")){
+            System.out.print("What is your name ");
+            scanner.nextLine();
+            username = scanner.nextLine();
+            System.out.println("Hello, " + username);
+        }
 
     }
 }
