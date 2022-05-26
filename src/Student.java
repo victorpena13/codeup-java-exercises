7d33c7c90fd4aba5a373c5338256037a6dd9651c
victor pena <victor.pena1392@Gmail.com>
2022-05-25 19:21:24 -0500

    Student class has an example of an overloaded constructor; two definitions that differ in parameter count

--- src/Student.java
+++ src/Student.java
@@ -1,29 +1,19 @@
 public class Student {
     public String name;
     public String cohort;
-    private double grade;
 
     public Student(String studentName) {
         name = studentName;
-        cohort = "unassigned";
+        cohort = "Unassigned";
     }
 
     public Student(String studentName, String assignedCohort) {
         name = studentName;
-        cohort = "unassigned";
+        cohort = assignedCohort;
     }
 
     public String getInfo() {
         return String.format("name: %s, cohort: %s", name, cohort);
-    }
-
-    public Student(String name, double grade) {
-        this.name = name;
-        this. grade = grade;
-    }
-
-    public double shareGrade() {
-        return grade;
     }
 
     public static void main(String[] args) {
@@ -32,8 +22,5 @@ public class Student {
         System.out.println(s1.getInfo());
         System.out.println(s2.getInfo());
 
-        s1 = new Student(s1.name, 84);
-        System.out.println(s1.name);
-        System.out.println(s1.shareGrade());
     }
 }
