fc3225a7f971bb9ccb176f4b1988b6aef89f0453
victor pena <victor.pena1392@Gmail.com>
2022-05-26 09:16:57 -0500

    imported and used input class to capture users input

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,5 +1,18 @@
 package shapes;
 
+import util.Input;
 public class Circle {
     private double radius;
+
+    public void setRadius(double r) {
+        this.radius = r;
+    }
+
+    public static void main(String[] args) {
+        Input input = new Input();
+        System.out.print("what is the radius of circle: ");
+        String userRadius = input.getString();
+    }
+
+
 }
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -1,3 +1,5 @@
+package util;
+
 import util.Input;
 
 public class InputTest {
