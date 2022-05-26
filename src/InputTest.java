54f84a14784390468ba842b624a2404f14980a57
victor pena <victor.pena1392@Gmail.com>
2022-05-26 08:06:11 -0500

    getDouble method

--- src/InputTest.java
+++ src/InputTest.java
@@ -9,5 +9,6 @@ public class InputTest {
         String userInput = input.getString();
         System.out.println(input.yesNo(userInput));
         System.out.println(input.getInt(1,10));
+        System.out.println(input.getDouble(1,20));
     }
 }
--- src/util/Input.java
+++ src/util/Input.java
@@ -19,7 +19,7 @@ public class Input {
 
     public int getInt(int min, int max) {
             Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter a number between 1 and 10: ");
+            System.out.print("Enter a number between " + min + " and " + max);
             int userInput = scanner.nextInt();
             if (userInput < min) {
                 System.out.println("the number:" + userInput + " is smaller then " + min);
@@ -29,5 +29,19 @@ public class Input {
                 getInt(min, max);
             }
             return userInput;
+    }
+
+    public double getDouble(double min, double max) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number between " + min + " and " + max);
+        double userInput = scanner.nextDouble();
+        if (userInput < min) {
+            System.out.println("the number:" + userInput + " is smaller then " + min);
+            getDouble(min, max);
+        } else if (userInput > max) {
+            System.out.println("the number:" + userInput + " is bigger then " + max);
+            getDouble(min, max);
+        }
+        return userInput;
     }
 }
