311ef544c90eff87d00f7a23ba80be798703749b
victor pena <victor.pena1392@Gmail.com>
2022-05-26 17:29:15 -0500

    used an instance of circle in CircleApp to caclculate the area & circumference

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,8 +1,11 @@
 package shapes;
 
-import util.Input;
 public class Circle {
     private double radius;
+
+    public double getRadius() {
+        return radius;
+    }
 
     public void setRadius(double r) {
         this.radius = r;
@@ -11,11 +14,7 @@ public class Circle {
     public double getArea(double radius) {
         return Math.PI * (radius * radius) ;
     }
-
-
-    public static void main(String[] args) {
-
+    public double getCircumference(double radius) {
+        return 2 * Math.PI * radius;
     }
-
-
 }
--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -1,11 +1,16 @@
 package shapes;
-
 import util.Input;
+import static java.lang.Double.parseDouble;
 
 public class CircleApp {
     public static void main(String[] args) {
+        Circle circle = new Circle();
         Input input = new Input();
         System.out.print("what is the radius of circle: ");
         String userRadius = input.getString();
+        circle.setRadius(parseDouble(userRadius));
+        System.out.println("radius: " + circle.getRadius());
+        System.out.println("area: " + circle.getArea(circle.getRadius()));
+        System.out.println("circumference: " + circle.getCircumference(circle.getRadius()));
     }
 }
