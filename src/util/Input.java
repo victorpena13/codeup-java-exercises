47c43ea139d338bcf24d2562a5c82571974e5035
victor pena <victor.pena1392@Gmail.com>
2022-05-27 10:36:33 -0500

    updated yesNo method to loop until user states otherwise by using .equals and .startsWith methods inside a while loop

--- src/util/Input.java
+++ src/util/Input.java
@@ -9,26 +9,31 @@ public class Input {
         return scanner.nextLine();
     }
 
-    public boolean yesNo(String userInput) {
-        if (userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("yes")) {
-            return true;
-        } else {
-            return false;
+    public void yesNo(String userInput) {
+        Input input = new Input();
+        while (userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("yes")) {
+            System.out.println("yes or no: ");
+            userInput = input.getString();
+            yesNo(userInput);
+            if(userInput.startsWith("no") || userInput.startsWith("n")) {
+                System.out.println("goodbye");
+                return;
+            }
         }
     }
 
     public int getInt(int min, int max) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter a number between " + min + " and " + max);
-            int userInput = scanner.nextInt();
-            if (userInput < min) {
-                System.out.println("the number:" + userInput + " is smaller then " + min);
-                getInt(min, max);
-            } else if (userInput > max) {
-                System.out.println("the number:" + userInput + " is bigger then " + max);
-                getInt(min, max);
-            }
-            return userInput;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number between " + min + " and " + max);
+        int userInput = scanner.nextInt();
+        if (userInput < min) {
+            System.out.println("the number:" + userInput + " is smaller then " + min);
+            getInt(min, max);
+        } else if (userInput > max) {
+            System.out.println("the number:" + userInput + " is bigger then " + max);
+            getInt(min, max);
+        }
+        return userInput;
     }
 
     public double getDouble(double min, double max) {
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -9,7 +9,7 @@ public class InputTest {
         Input input = new Input();
         System.out.print("yes or no? ");
         String userInput = input.getString();
-        System.out.println(input.yesNo(userInput));
+        input.yesNo(userInput);
         System.out.println(input.getInt(1,10));
         System.out.println(input.getDouble(1,20));
     }
