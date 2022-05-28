18387ac421bdd452b9bd53691dbc493e570742b3
victor pena <victor.pena1392@Gmail.com>
2022-05-28 12:33:11 -0500

    created a class inside src named ServerNameGenerator

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -2,6 +2,13 @@ import java.util.Arrays;
 
 public class ArraysExercises {
 
+    public static String addPerson(Person[] persons, Person person) {
+        Arrays.copyOf(persons, persons.length + 2);
+        persons[persons.length-1] = person;
+        return Arrays.toString(persons);
+
+    }
+
     public static void main(String[] args) {
         int[] test={1,2,4};
         int[] numbers={1,2,3,4,5};
@@ -14,8 +21,11 @@ public class ArraysExercises {
         victor.setName("victor");
         vic.setName("vic");
         v.setName("v");
+        Person vktr = new Person();
+        vktr.setName("vktr");
 
         Person[] persons = {victor, vic, v};
+        addPerson(persons,vktr);
         for(Person person: persons) {
             System.out.println(person.getName());
         }
