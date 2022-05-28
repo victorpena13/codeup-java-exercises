0a8da9d42cf377a761e45dbdac9c6b9cf203a9bd
victor pena <victor.pena1392@Gmail.com>
2022-05-27 23:14:58 -0500

    created ArraysLesson class

---
+++ src/ArraysLesson.java
@@ -0,0 +1,6 @@
+public class ArraysLesson {
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/util/Input.java
+++ src/util/Input.java
@@ -1,7 +1,5 @@
 package util;
-
 import shapes.Circle;
-
 import java.util.Scanner;
 
 public class Input {
