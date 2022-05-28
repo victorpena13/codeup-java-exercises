505f3c6da62f809e807f62f4912ef8daa3b2a797
victor pena <victor.pena1392@Gmail.com>
2022-05-28 00:03:26 -0500

    the beatles arrays size and elements can both be defined at the same time

--- src/ArraysLesson.java
+++ src/ArraysLesson.java
@@ -1,7 +1,25 @@
 public class ArraysLesson {
     public static void main(String[] args) {
 
+        //arrays have a fixed length.
         double [] prices;
+        prices = new double[4];
+
+        int[] numbers = new int[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        System.out.println(numbers[0]); // 1
+        System.out.println(numbers[1]); // 2
+        System.out.println(numbers[2]); // 0;
+        // System.out.println(numbers[3]); // ArrayIndexOutOfBoundsException
+
+        String[] beatles = {"John", "Paul", "Ringo"};
+        System.out.println(beatles.length); // 3
+        System.out.println(beatles[0]); // "John"
+        System.out.println(beatles[1]); // "Paul"
+        System.out.println(beatles[2]); // "Ringo"
+
+        // beatles[3] = "George"; // ArrayIndexOutOfBoundsException
 
     }
 }
