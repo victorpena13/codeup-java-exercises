f01894976dd007636bb3be94b4b8281e4e1d99a9
victor pena <victor.pena1392@Gmail.com>
2022-05-29 00:52:51 -0500

    created two array whose elements are adjectives and nouns

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,2 +1,9 @@
 public class ServerNameGenerator {
+
+    public static void main(String[] args) {
+        String[] adjectives = {"charming", "cruel", "fantastic", "gentle", "huge", "perfect", "rough", "sharp"};
+        String[] nouns = {"cats", "sock", "ships", "heroes", "monkeys", "babies", "matches"};
+
+
+    }
 }
