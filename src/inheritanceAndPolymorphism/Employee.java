7e0deeeb817731b440a3e78541b7436ef472e852
victor pena <victor.pena1392@Gmail.com>
2022-06-01 11:37:58 -0500

    extending subclasses

--- src/inheritanceAndPolymorphism/Employee.java
+++ src/inheritanceAndPolymorphism/Employee.java
@@ -1,13 +1,25 @@
 package inheritanceAndPolymorphism;
 
 public class Employee extends Person{
+    private double salary;
+
+    public void doWork() {
+        System.out.println("Work, work...");
+    }
+
     public Employee(String employeeName) {
         super(employeeName);
     }
 
     public static void main(String[] args) {
+        Person p = new Person("P");
         Employee john = new Employee("John");
         john.sayHello();
+        john.doWork();
         System.out.println(john.name);
+        p.sayHello();
+//        p.doWork();  //superclasses do not extend subclasses.
+
+
     }
 }
