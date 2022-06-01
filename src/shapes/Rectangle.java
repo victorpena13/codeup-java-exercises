3ff0d864d15634e321da26e5081d0440b57d2844
victor pena <victor.pena1392@Gmail.com>
2022-06-01 13:01:48 -0500

    inside shapes directory, created a class named Rectangle. it has protected properties for length and width. rectangle defines a constructor that accepts two numbers for length and width, and sets those properties. created two methods on the rectangle class, getArea and getPerimeter

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,21 @@
+package shapes;
+
+public class Rectangle {
+    protected double length;
+    protected double width;
+
+    public void Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getArea() {
+        return this.length * this.width;
+    }
+
+    public double getPerimeter() {
+        return this.length + this.length + this.width + this.width;
+    }
+
+
+}
