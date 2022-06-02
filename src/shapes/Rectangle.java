5f5f80602cccb2d218a94d9cf3230a52bd2dbdbc
victor pena <victor.pena1392@Gmail.com>
2022-06-02 15:59:47 -0500

    removed the code from the bodies of Rectangle and Square classes

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,22 +1,5 @@
 package shapes;
 
 public class Rectangle {
-    protected double length;
-    protected double width;
-
-
-    public Rectangle(double length, double width) {
-        this.length = length;
-        this.width = width;
-    }
-
-    public double getArea() {
-        return this.length * this.width;
-    }
-
-    public double getPerimeter() {
-        return this.length + this.length + this.width + this.width;
-    }
-
 
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,7 +1,5 @@
 package shapes;
 
 public class Square extends Rectangle{
-    public Square(double side) {
-        super(side,side);
-    }
+
 }
