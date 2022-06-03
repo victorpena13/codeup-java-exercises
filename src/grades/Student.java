a04565f08dc839f6467a7e37a4291394a4c5e933
victor pena <victor.pena1392@Gmail.com>
2022-06-03 17:38:02 -0500

    the class student has private properties for name and grades. the grades property is an ArrayList of Integers. the student class has a constructor that sets name property and initializes the grades property as an empty ArrayList.

---
+++ src/grades/Student.java
@@ -0,0 +1,16 @@
+package grades;
+
+import java.util.ArrayList;
+
+public class Student {
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student(String name) {
+        this.name = name;
+        grades = new ArrayList<>();
+    }
+
+
+
+}
