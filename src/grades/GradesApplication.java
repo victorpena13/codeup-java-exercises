431b4c31b5b97ae6473474a20161aa6ce5b21f3e
victor pena <victor.pena1392@Gmail.com>
2022-06-04 14:52:00 -0500

    created 4 student objects with at least 3 grades 3ach and added them to the hashmap students

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -4,10 +4,23 @@ import java.util.HashMap;
 public class GradesApplication {
 
     public static void main(String[] args) {
-        Student victor = new Student("victor pena");
+        Student victor = new Student("victor");
+        Student clay = new Student("clay");
+        Student elvis = new Student("elvis");
         HashMap<String, String> students = new HashMap<>();
+        victor.addGrade(25);
+        victor.addGrade(100);
+        victor.addGrade(100);
+        clay.addGrade(75);
+        clay.addGrade(75);
+        clay.addGrade(80);
+        elvis.addGrade(40);
+        elvis.addGrade(100);
+        elvis.addGrade(100);
 
         students.put(victor.getName() , "victorpena13");
+        students.put(clay.getName() , "apeclay92");
+        students.put(elvis.getName() , "elvispresley1");
         System.out.println(students.get("victor pena"));
 
 
