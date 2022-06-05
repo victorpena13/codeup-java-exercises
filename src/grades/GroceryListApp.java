d4e75cf89ecda00b659ff206e1877e1e546fa807
victor pena <victor.pena1392@Gmail.com>
2022-06-05 00:51:19 -0500

    started grocerylist app with if and else statements

--- src/grades/GroceryListApp.java
+++ src/grades/GroceryListApp.java
@@ -1,4 +1,24 @@
 package grades;
 
+import util.Input;
+
 public class GroceryListApp {
+
+
+
+    public static void main(String[] args) {
+        Input input = new Input();
+        System.out.println("Welcome!");
+        System.out.print("Would you like to create a Grocery List? [y/n]");
+        String userResponse = input.getString();
+        if(userResponse.equalsIgnoreCase("y")) {
+            System.out.print("\nWould you like to enter a new item? [y/n]");
+            userResponse = input.getString();
+            if(userResponse.equalsIgnoreCase("y")) {
+                System.out.println();
+            }
+        }
+
+
+    }
 }
