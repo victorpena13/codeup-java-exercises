cf9f41b4b77c8a70677a4de180117716839c209c
victor pena <victor.pena1392@Gmail.com>
2022-06-04 23:49:09 -0500

    added an attendance property to Student object.  it is a HashMap.  the keys are string, string representing date and p-present & a-absent

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,10 +1,12 @@
 package grades;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Student extends StudentTest {
     private String name;
     private ArrayList<Integer> grades;
+    private HashMap<String, String> attendance;
 
     public Student(String name) {
         this.name = name;
