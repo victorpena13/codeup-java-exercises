f80f73482bfe144cba27b6fc6763819ee7c1b1ce
victor pena <victor.pena1392@Gmail.com>
2022-06-10 11:02:47 -0500

    used a collection for every value of map to store multiple values to one key

--- src/hashmaps.java
+++ src/hashmaps.java
@@ -1,6 +1,20 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class hashmaps {
 
     public static void main(String[] args) {
+        Map<String, List<String>> map = new HashMap<>();
+        List<String> list = new ArrayList<>();
+        map.put("key1", list);
+        map.get("key1").add("value1");
+        map.get("key1").add("value2");
+
+
+
+        System.out.println(map.get("key1"));
 
     }
 }
