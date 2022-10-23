160c363f3eb4da419149b6ff55437fd03b072557
Victor Pena <13victor.pena@gmail.com>
2022-10-23 10:10:43 -0500

    created int string and long variables and printed them to the console

--- src/HelloVictor.java
+++ src/HelloVictor.java
@@ -2,5 +2,18 @@ public class HelloVictor {
 
     public static void main(String[] args) {
         System.out.println("Hello, Victor");
+
+        int myFavoriteNumber = 13;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "this is a string";
+        System.out.println(myString);
+        myString = "a";
+        System.out.println(myString);
+
+        long myNumber = 13;
+        System.out.println(myNumber);
+
+
     }
 }
