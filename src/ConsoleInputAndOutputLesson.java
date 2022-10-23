1ceef6cc0b5e5c19318b804e9ce10e341d6129d4
Victor Pena <13victor.pena@gmail.com>
2022-10-23 12:46:04 -0500

    scanner class to grab user input

--- src/ConsoleInputAndOutputLesson.java
+++ src/ConsoleInputAndOutputLesson.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class ConsoleInputAndOutputLesson {
     public static void main(String[] args) {
         String firstName = "victor";
         System.out.printf("Hello there %s, Nice to see you. %n", firstName);
+
+        String greeting = "Salutations";
+        System.out.printf("%s, %s!", greeting, firstName);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter something: ");
+        String userInput = scanner.next();
+        System.out.print("You enetered: -->\"" + userInput + "\" <--");
+
+
     }
 }
