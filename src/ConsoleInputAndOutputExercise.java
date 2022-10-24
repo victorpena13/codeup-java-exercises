676c0da20522ffd963aad06990ca2d1199d67261
Victor Pena <13victor.pena@gmail.com>
2022-10-23 22:42:20 -0500

    used scanner.next and scanner.nextLine to capture words and sentences

--- src/ConsoleInputAndOutputExercise.java
+++ src/ConsoleInputAndOutputExercise.java
@@ -1,7 +1,28 @@
+import java.util.Scanner;
+
 public class ConsoleInputAndOutputExercise {
 
     public static void main(String[] args) {
-                double pi = 3.14159;
-                System.out.printf("two decimal places: %.2f ", pi);
+        double pi = 3.14159;
+        System.out.printf("two decimal places: %.2f ", pi);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println();
+        System.out.print("enter a number: ");
+        int userNumber = scanner.nextInt();
+        System.out.println(userNumber);
+        System.out.println("please enter three words: ");
+
+        String wordOne = scanner.next();
+        String wordTwo = scanner.next();
+        String wordThree = scanner.next();
+
+        System.out.printf("%s\n%s\n%s\n" , wordOne, wordTwo, wordThree);
+
+        System.out.print("enter a sentence: ");
+        String userSentence = scanner.nextLine();
+        System.out.println(userSentence);
+
+
     }
 }
