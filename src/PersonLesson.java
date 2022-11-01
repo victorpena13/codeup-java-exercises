21271bc21655045eed98581b8ad664b47b528ebb
Victor Pena <13victor.pena@gmail.com>
2022-11-01 15:06:36 -0500

    defined a class named PersonLesson with two instance variables or fields, firstname & lastname. the class also defines an instance method, sayHello

--- src/PersonLesson.java
+++ src/PersonLesson.java
@@ -1,3 +1,16 @@
 public class PersonLesson {
+    public String firstName;
+    public String lastName;
+
+    public String sayHello() {
+        return String.format("Hello from %s %s!", firstName, lastName);
+    }
+
+    public static void main(String[] args) {
+        PersonLesson rick = new PersonLesson();
+        rick.firstName = "rick";
+        rick.lastName = "sanchez";
+        System.out.println(rick.sayHello());
+    }
 
 }
