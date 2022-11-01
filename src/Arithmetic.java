6e9bd2e6dcbf040c8266016a575cb8fcd3a8d7b8
Victor Pena <13victor.pena@gmail.com>
2022-11-01 15:21:45 -0500

    created a class arithmetic that has static fields

--- src/Arithmetic.java
+++ src/Arithmetic.java
@@ -1,15 +1,11 @@
 public class Arithmetic {
-
     public static double pi = 3.14159;
 
     public static int add(int x, int y) {
         return x + y;
     }
 
-    public static int multiply(int x, int y) {
+    public static int multiply (int x, int y) {
         return x * y;
     }
-
-    //the arithmetic class is only meant to be referenced statically, meaning that we never create instances of it;
-
 }
