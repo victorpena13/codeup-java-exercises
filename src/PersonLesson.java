a22845a7fdef8dc2ed7da7ad51695c50702c4d21
Victor Pena <13victor.pena@gmail.com>
2022-11-01 16:02:01 -0500

    added instance vs static examples and a constructor

--- src/PersonLesson.java
+++ src/PersonLesson.java
@@ -1,10 +1,17 @@
 public class PersonLesson {
+    public static long worldPopulation = 7_500_000_000L;
+
     public String firstName;
     public String lastName;
+
+    public PersonLesson() {
+        System.out.println("A person is being created");
+    }
 
     public String sayHello() {
         return String.format("Hello from %s %s!", firstName, lastName);
     }
+
 
     public static void main(String[] args) {
         PersonLesson rick = new PersonLesson();
@@ -22,6 +29,14 @@ public class PersonLesson {
 
         System.out.println(ada.sayHello());
         System.out.println(grace.sayHello());
-    }
 
+        PersonLesson theBestDrummerAlive = new PersonLesson();
+        theBestDrummerAlive.firstName = "Travis";
+        theBestDrummerAlive.lastName = "Barker";
+
+        PersonLesson.worldPopulation += 1;
+
+        System.out.println(PersonLesson.worldPopulation);
+        System.out.println(theBestDrummerAlive.worldPopulation);
+    }
 }
