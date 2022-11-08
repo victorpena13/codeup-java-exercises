b712c28ad52e8427c44821a0641728505ce6b457
Victor Pena <13victor.pena@gmail.com>
2022-11-07 21:35:36 -0600

    used .equals method

--- src/Person.java
+++ src/Person.java
@@ -12,4 +12,16 @@ public class Person {
     public void sayHello() {
         System.out.println("hello from, " + this.name);
     }
+
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        person1.setName("john");
+        person1.getName();
+        person1.sayHello();
+
+        Person person2 = new Person();
+        person2.setName("victor");
+
+        System.out.println(person1.getName().equals(person2.getName()));
+    }
 }
