940aae47fb97a8c529b91562c160d8cf35726a4f
Victor Pena <13victor.pena@gmail.com>
2022-11-14 21:19:36 -0600

    created an int variable and printed it to the console

--- src/HelloEarth.java
+++ src/HelloEarth.java
@@ -1,5 +1,8 @@
 public class HelloEarth {
     public static void main(String[] args){
         System.out.println("Hello, Earth");
+
+        int myFavoriteNumber = 7;
+        System.out.println(myFavoriteNumber);
     }
 }
