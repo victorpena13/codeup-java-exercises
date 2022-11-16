f03bf7e517fd7b43201eb37acb8536f65ab1d0fb
Victor Pena <13victor.pena@gmail.com>
2022-11-16 15:32:20 -0600

    used a do while loop to iterate in multiples of 2 from 0 to 100 following a new line with each number

--- src/ControlStatementsLoops.java
+++ src/ControlStatementsLoops.java
@@ -6,5 +6,11 @@ public class ControlStatementsLoops {
             System.out.print(i + " ");
             i++;
         }
+
+        int a = 0;
+        do {
+            System.out.println(a);
+            a+=2;
+        } while(a<=100);
     }
 }
