fb6cf95d14fa7c845db50099565843e82cf039a8
Victor Pena <13victor.pena@gmail.com>
2022-11-22 21:48:43 -0600

    multiplyRecursion)

--- src/Methods.java
+++ src/Methods.java
@@ -1,5 +1,4 @@
 public class Methods {
-
     public static int sum(int x, int y) {
         return x + y;
     }
@@ -11,8 +10,6 @@ public class Methods {
     public static int subtract(int x, int y) {
         return x -y;
     }
-
-
     public static int multiplyLoop(int x , int y) {
         int power= 0;
         for (int i = 1; i <= y; i++) {
@@ -22,10 +19,20 @@ public class Methods {
         return x;
     }
 
-
-
+    public static int multiplyRecursion(int x, int y) {
+        if (x == 0 || y == 0) {
+            return 0;
+        } else if (x == 1) {
+            return y;
+        } else if (y == 1) {
+            return y;
+        } else {
+            return multiplyRecursion(x-1, y) +y;
+        }
+    }
     public static void main(String[] args) {
 
         System.out.println(multiplyLoop(4, 5));
+        System.out.println(multiplyRecursion(5, 6));
     }
 }
