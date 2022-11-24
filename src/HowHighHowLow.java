16c369a069e887da0a7205adc19765fdfe320d60
Victor Pena <13victor.pena@gmail.com>
2022-11-24 10:35:03 -0600

    completed highlow game

--- src/HowHighHowLow.java
+++ src/HowHighHowLow.java
@@ -6,14 +6,27 @@ public class HowHighHowLow {
         Scanner scanner = new Scanner(System.in);
         System.out.println("pick a number 1 through 100.");
         int userInteger;
-        do {
-            System.out.print("guess number: ");
-            userInteger = scanner.nextInt();
-            if (userInteger == number) {
-                System.out.println("you did it!");
-            }
+        int count=-1;
+            do {
+                if (count >= 5) {
+                    System.out.println("You lose");
+                    break;
+                }
+                System.out.print("guess number: ");
+                userInteger = scanner.nextInt();
+                count++;
+                if(userInteger > number) {
+                    System.out.println("lower");
+                } else if(userInteger < number) {
+                    System.out.println("higher");
+                } else if (userInteger == number) {
+                    count++;
+                    System.out.println("you did it!");
+                    System.out.println("counts: " + count);
+                }
+            }while(userInteger != number && count <= 5);
+        System.out.println("your count is "+ count);
 
-        }while(userInteger != number);
     }
 
     public static void main(String[] args) {
@@ -21,6 +34,7 @@ public class HowHighHowLow {
         int low = 1;
         int high = 100;
         int result = r.nextInt(high-low) + low;
+        System.out.println(result);
 
         highLow(result);
     }
