84a41baefbf9b4732170e2d75bbd2ce80776418b
Victor Pena <13victor.pena@gmail.com>
2022-11-26 20:08:03 -0600

    arrays review

---
+++ src/Arrays2022.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class Arrays2022 {
+
+    public static void main(String[] args) {
+        int[] numbers={1,2,3,4,5};
+        System.out.println(numbers);
+        System.out.println(Arrays.toString(numbers));
+    }
+}
---
+++ src/ArraysLesson2022.java
@@ -0,0 +1,18 @@
+public class ArraysLesson2022 {
+
+    public static void main(String[] args) {
+
+        double[] prices;
+        prices = new double[4];
+
+        int[] numbers = new int[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        System.out.println(numbers[0]); //1
+        System.out.println(numbers[1]); //2
+        System.out.println(numbers[2]); //0 - default vaule
+//        System.out.println(numbers[3]); //ArrayIndexOutOfBoundsException
+
+
+    }
+}
