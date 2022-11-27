013507b53daddac4d411c1647d2b817e3056131c
Victor Pena <13victor.pena@gmail.com>
2022-11-26 20:46:27 -0600

    iterating through arrays with for loops and exploring static methods in the arrays class

--- src/ArraysLesson2022.java
+++ src/ArraysLesson2022.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class ArraysLesson2022 {
 
     public static void main(String[] args) {
@@ -12,6 +14,32 @@ public class ArraysLesson2022 {
         System.out.println(numbers[1]); //2
         System.out.println(numbers[2]); //0 - default vaule
 //        System.out.println(numbers[3]); //ArrayIndexOutOfBoundsException
+
+        String[] beatles = {"john", "paul", "ringo"};
+        System.out.println(beatles.length);
+
+        System.out.println(beatles[0]);
+        System.out.println(beatles[1]);
+        System.out.println(beatles[2]);
+
+        for(int i=0; i <beatles.length; i+=1) {
+            System.out.println(beatles[i]);
+        }
+
+        String[] languages = {"html", "css", "javascript", "java"};
+
+        for(String language : languages) {
+            System.out.println(language);
+        }
+
+        String[] letters = {"a", "b", "c", "d", "e"};
+        for(String l : letters) {
+            System.out.println(l);
+        }
+
+        Arrays.fill(numbers,10);
+        System.out.println(Arrays.toString(numbers));
+
 
 
     }
