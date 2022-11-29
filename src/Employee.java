406d9454505ae6fcfa8df0d4783768185f460867
Victor Pena <13victor.pena@gmail.com>
2022-11-28 21:01:19 -0600

    because employee extends personlesson class. employee objects can use the sayhello method, person constructor and name property

--- src/Employee.java
+++ src/Employee.java
@@ -2,4 +2,10 @@ class Employee extends PersonLesson {
     public Employee(String employeeFirstName, String employeeLastName){
         super(employeeFirstName, employeeLastName);
     }
+
+    public static void main(String[] args) {
+        Employee taylor = new Employee("taylor", "swift");
+        System.out.println(taylor.sayHello());
+        System.out.println(taylor.firstName + " " + taylor.lastName);
+    }
 }
