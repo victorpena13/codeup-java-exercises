005c4dfc9d30bfae2f8dd8577d4053cfe5e7be0c
Victor Pena <13victor.pena@gmail.com>
2023-01-24 21:43:22 -0600

    object example with PersonLesson.java with notes

--- src/PersonLesson.java
+++ src/PersonLesson.java
@@ -1,64 +1,23 @@
 public class PersonLesson {
-    public static long worldPopulation = 7_500_000_000L;
 
-    private String firstName;
-    private String lastName;
-    private int age;
+    // class - is used t o define a custom type in java. blue print for objects to be created on it. 
+    // field - variable or method that belongs to an object or a class. 
+    // object - is an instance of a class 
+    //        - have field and methods, and are instantiated, created with the new keyword.
 
-    public PersonLesson(String first_name, String last_name) {
-        this.firstName = first_name;
-        this.lastName = last_name;
-
-    }
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public int getAge() {
-        return this.age;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return this.lastName;
-    }
-
-    public PersonLesson() {
-        System.out.println("A person is being created");
-    }
+    public String firstName; 
+    public String lastName;
 
     public String sayHello() {
-        return String.format("Hello from %s %s!", this.firstName, this.lastName);
+        return String.format("hello from %s %s!", firstName, lastName);
     }
 
-
     public static void main(String[] args) {
+
         PersonLesson rick = new PersonLesson();
         rick.firstName = "rick";
         rick.lastName = "sanchez";
         System.out.println(rick.sayHello());
 
-        PersonLesson ada = new PersonLesson();
-        ada.firstName = "ada";
-        ada.lastName = "lovelace";
-
-        PersonLesson grace = new PersonLesson();
-        grace.firstName = "grace";
-        grace.lastName = "hopper";
-
-        System.out.println(ada.sayHello());
-        System.out.println(grace.sayHello());
-
-        PersonLesson theBestDrummerAlive = new PersonLesson();
-        theBestDrummerAlive.firstName = "Travis";
-        theBestDrummerAlive.lastName = "Barker";
-
-        PersonLesson.worldPopulation += 1;
-
-        System.out.println(PersonLesson.worldPopulation);
-        System.out.println(theBestDrummerAlive.worldPopulation);
     }
 }
